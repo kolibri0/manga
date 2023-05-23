49108e51b60477f5f6c3e48247e57276d86118da
kolibri0 <ddr5xxa@mail.ru>
2023-05-23 20:43:15 +0300

    change responsive

--- components/ItemUpContent.tsx
+++ components/ItemUpContent.tsx
@@ -9,7 +9,8 @@ const ItemUpContent = ({ styles, content, redirectToGenres }) => {
     <div className={styles.infoTitle}>
       <div className={styles.title}>{content.title}</div>
       <div className={styles.genres}>{content.genres.map((genre) => <div className={styles.genre} onClick={() => redirectToGenres(genre.mal_id)}>{genre.name}</div>)}</div>
-      <div className={styles.synopsis}>{content.synopsis.length < 900 ? content.synopsis : (content.synopsis.slice(0, 900) + ' ...')}</div>
+      {/* <div className={styles.synopsis}>{content.synopsis.length < 900 ? content.synopsis : (content.synopsis.slice(0, 900) + ' ...')}</div> */}
+      <div className={styles.synopsis}>{content.synopsis}</div>
     </div>
   </>);
 }
